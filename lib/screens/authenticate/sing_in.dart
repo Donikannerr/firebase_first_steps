04b465f43c013b442e3370936c13019738f40b09
Donikannerr <67806377+Donikannerr@users.noreply.github.com>
2021-05-13 00:59:30 +0200

    add sign_in_anonym

---
+++ lib/screens/authenticate/sing_in.dart
@@ -0,0 +1,40 @@
+import 'package:firebase_first_steps/services/auth.dart';
+import 'package:flutter/material.dart';
+
+class SignIn extends StatefulWidget {
+  @override
+  _SignInState createState() => _SignInState();
+}
+
+class _SignInState extends State<SignIn> {
+  // erstell ein objekt der Klasse AuthService
+  final AuthService _auth = new AuthService();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.brown[100],
+      appBar: AppBar(
+        backgroundColor: Colors.brown[400],
+        elevation: 0.0,
+        title: Text('Sign in'),
+      ),
+      body: Container(
+        padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 50.0),
+        child: ElevatedButton(
+          child: Text('Sign in anon'),
+          // Wenn auf Knopf gedrückt wird, führe Funktion signInAnon aus
+          onPressed: () async {
+            dynamic result = await _auth.signInAnon();
+            if (result == null) {
+              print('error signing in');
+            } else {
+              print('signing in');
+              print(result);
+            }
+          },
+        ),
+      ),
+    );
+  }
+}
--- lib/screens/wrapper.dart
+++ lib/screens/wrapper.dart
@@ -1,10 +1,10 @@
-import 'package:firebase_first_steps/screens/home/home.dart';
+import 'package:firebase_first_steps/screens/authenticate/authenticate.dart';
 import 'package:flutter/material.dart';
 
 class Wrapper extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     //return either Home or Authenticate widget
-    return new Home();
+    return new Authenticate();
   }
 }
